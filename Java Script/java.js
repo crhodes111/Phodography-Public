d8e33645e78ab03c4b6365f5016a5faa6f4bd1b4
crhodes111 <86021107+crhodes111@users.noreply.github.com>
2021-10-09 22:01:17 -0400

    Add files via upload

---
+++ Java Script/java.js
@@ -0,0 +1,201 @@
+ var allisoncoltArray =  ["./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-03.jpg",
+ "./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-05.jpg",
+ "./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-07.jpg",
+ "./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-15.jpg",
+ "./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-22.jpg",
+ "./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-28.jpg",
+ "./wetransfer_doggos_2021-09-14_0428/Phodography/Allison & Colt/A&C-32.jpg"
+]
+var catherineBallerArray = ["./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-03.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-09.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-13.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-22.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-24.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-28.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-33.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Catherine & Baller/C&B-36.jpg"
+]
+var courtneyMaddieArray = ["./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-117.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-119.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-123.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-124.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-128.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-130.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-134.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Courtney_Maddie & Marley/A&C-139.jpg",]
+var chloeTysonArray = ["./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1604.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1604.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1632.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1648.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1655.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1660.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1680.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Chloe, Tyson, Molly & Maggie/268A1685.JPG"]
+var jenanMJArray = ["./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6761.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6765.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6784.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6843.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6873.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6886.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6894.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6900.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Jenan & MJ/268A6923.JPG"]
+
+var stephanieSonniArray = ["./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-02.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-05.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-07.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-12.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-17.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-22.jpg",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Stephanie & Sonni/Steph_Sonni-25.jpg",
+]
+var leisure = ["./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A0747.jpg.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A6962.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A6994.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A0254.jpg.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A5748.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A2657.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A5144.JPG",
+"./wetransfer_doggos_2021-09-14_0428/Phodography/Leisure/268A0232.jpg.JPG"
+]
+var leisureslides = document.getElementById("leisureslides");
+var f = 0
+$('#nextbuttonl').on('click', 'span',  function (e) {
+    f++
+
+if(f>7){
+    f = 0
+}
+leisureslides.setAttribute("src", leisure[f]);
+});
+$('#prevbuttonl').on('click', 'span',  function (e) {
+    if(f<1){
+        f = 1
+    }
+    f--
+leisureslides.setAttribute("src", leisure[f]);
+
+});
+var acslides = document.getElementById("a&cimage");
+var i = 0
+$('#nextbuttonac').on('click', 'span',  function (e) {
+    i++
+acslides.setAttribute("src", allisoncoltArray[i]);
+if(i>6){
+    i = 0
+}
+});
+$('#prevbuttonac').on('click', 'span',  function (e) {
+    if(i<1){
+        i = 1
+        
+    }
+
+   i--
+acslides.setAttribute("src", allisoncoltArray[i]);
+
+});
+
+var cbslides = document.getElementById("c&bimage");
+var j = 0
+$('#nextbuttoncb').on('click', 'span',  function (e) {
+    j++
+cbslides.setAttribute("src", catherineBallerArray[j]);
+
+if(j>7){
+    j = 0
+}
+});
+$('#prevbuttoncb').on('click', 'span',  function (e) {
+   
+if(j<1){
+    j = 1
+}
+j--
+cbslides.setAttribute("src", catherineBallerArray[j]);
+});
+var cmslides = document.getElementById("c&mimage");
+var l = 0
+$('#nextbuttoncm').on('click', 'span',  function (e) {
+l++
+cmslides.setAttribute("src", courtneyMaddieArray[l]);
+
+if(l>7){
+    l = 0
+}
+});
+$('#prevbuttoncm').on('click', 'span',  function (e) {
+    
+
+if(l<1){
+    l = 1
+}
+l--
+cmslides.setAttribute("src", courtneyMaddieArray[l]);
+});
+
+var ctslides = document.getElementById("c&timage");
+var z = 0
+$('#nextbuttonct').on('click', 'span',  function (e) {
+    z++
+ctslides.setAttribute("src", chloeTysonArray[z]);
+if(z>7){
+    z = 0
+}
+});
+$('#prevbuttonct').on('click', 'span',  function (e) {
+    
+if(z<1){
+    z= 1
+}
+z--
+ctslides.setAttribute("src", chloeTysonArray[z]);
+});
+
+var jmslides = document.getElementById("j&mimage");
+
+var m = 0
+$('#nextbuttonjm').on('click', 'span',  function (e) {
+    m++
+jmslides.setAttribute("src", jenanMJArray[m]);
+
+if(m>7){
+    m = 0
+}
+});
+
+$('#prevbuttonjm').on('click', 'span',  function (e) {
+    
+
+if(m<1){
+    m = 1
+}
+m--
+ctslides.setAttribute("src", jenanMJArray[m]);
+});
+
+
+  /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction2() {
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }  
+  function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less";
+      moreText.style.display = "inline";
+    }
+  }
